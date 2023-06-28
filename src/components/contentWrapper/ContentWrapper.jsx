4e6641c1c6c33d83c863f93b91dd0e5dd991f19b
Aniket gupta <52057853+aniket-a@users.noreply.github.com>
2023-06-28 15:46:09 +0530

    update

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./contentwrapper.scss"
+const ContentWrapper = ({children}) => {
+  return (
+    <div>
+      <div className="contentWrapper">{children}</div>
+    </div>
+  )
+}
+
+export default ContentWrapper
--- src/pages/home/heroBanner/HeroBaner.jsx
+++ src/pages/home/heroBanner/HeroBaner.jsx
@@ -1,45 +1,51 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "./heroBanner.scss"
 import { useNavigate } from 'react-router-dom'
-import UseFetch from '../../../Hooks/UseFetch'
 import { useSelector } from 'react-redux'
+import UseFetch from '../../../Hooks/UseFetch'
+import Img from '../../../components/lazyLoadImage/Img'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 
 const HeroBaner = () => {
-  const {url} = useSelector((state)=> state.home)
+  const { url } = useSelector((state) => state.home)
   const [background, setbackground] = useState("")
   const [query, setquery] = useState("")
 
-  const {data, loading} = UseFetch("/movie/upcoming")
+  const { data, loading } = UseFetch("/movie/upcoming")
 
-  useEffect(()=>{
-    const bg = url.backdrop + data?.results?.[Math.floor(Math.random(  ) * 20)]?.backdrop_path
+  useEffect(() => {
+    const bg = url.backdrop + data?.results[Math.floor(Math.random() * 20)]?.backdrop_path
     setbackground(bg)
-  },[data])
+  }, [data])
 
   const navigate = useNavigate()
 
-  function searchQueryHandler(e){
+  function searchQueryHandler(e) {
     e.preventDefault()
-    if(e.key === "Enter" && query.length > 0){
-      navigate(`/search/:${query}`)
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`)
     }
   }
 
   return (
     <div className='heroBanner'>
-      <div className="backdrop-img">
-          <img src={background} alt="" />
-      </div>
-        <div className="wrapper">
-            <div className="heroBannerContent">
-              <span className="title">Welcome</span>
-              <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
-              <div className="searchInput">
-                <input type="text" placeholder='Search for a movie and TV show...' onKeyUp={searchQueryHandler} onChange={(e)=> setquery(e.target.value)}/>
-                <button>Search</button>
-              </div>
+      {!loading && <div className="backdrop-img">
+        <Img src={background} alt="" />
+      </div>}
+
+      <div className="opacity-layer"></div>
+
+      <ContentWrapper>
+          <div className="heroBannerContent">
+            <span className="title">Welcome</span>
+            <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
+            <div className="searchInput">
+              <input type="text" placeholder='Search for a movie and TV show...' onKeyUp={searchQueryHandler} onChange={(e) => setquery(e.target.value)} />
+              <button className='btn_search'>Search</button>
             </div>
-        </div>
+          </div>
+      </ContentWrapper>
+
     </div>
   )
 }
